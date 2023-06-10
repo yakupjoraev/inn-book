e4d90fc7a9992ae73ece0d6bd4f84012122d9503
Yakup <yakupjoraev@gmail.com>
2023-06-10 04:00:07 +0300

    added new pages and new blocks

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,11 @@
 
 // Мобильное меню бургер
 function burgerMenu() {
+  const header = document.querySelector('header');
+
+  if (!header) {
+    return null
+  }
   const burger = document.querySelector('[data-burger]')
   const menu = document.querySelector('[data-menu]')
   const body = document.querySelector('body')
@@ -261,3 +266,218 @@ window.addEventListener('click', (event) => {
     event.target.classList.remove('show');
   }
 });
+
+function validateForm() {
+  // const container = document.querySelector('.form-data');
+
+  // if (!container) {
+  //   return null
+  // }
+
+  // Получаем значения полей
+  var firstName = document.querySelector('input[placeholder="Имя"]').value;
+  var lastName = document.querySelector('input[placeholder="Фамилия"]').value;
+  var phone = document.querySelector('input[type="tel"]').value;
+  var email = document.querySelector('input[type="email"]').value;
+  var password = document.querySelector('input[type="password"]').value;
+  var confirmPassword = document.querySelectorAll('input[type="password"]')[1].value;
+
+  // Проверка заполненности полей
+  if (firstName === '' || lastName === '' || phone === '' || email === '' || password === '' || confirmPassword === '') {
+    alert('Пожалуйста, заполните все поля.');
+    return false;
+  }
+
+  // Проверка формата email
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert('Пожалуйста, введите правильный адрес электронной почты.');
+    return false;
+  }
+
+  // Проверка формата телефона
+  var phoneRegex = /^\d{3}\s\d{2}\s\d{2}$/;
+  if (!phoneRegex.test(phone)) {
+    alert('Пожалуйста, введите правильный номер телефона (в формате: 999 99 99).');
+    return false;
+  }
+
+  // Проверка совпадения паролей
+  if (password !== confirmPassword) {
+    alert('Пароли не совпадают.');
+    return false;
+  }
+
+  // Если все проверки пройдены успешно, можно отправить форму
+  return true;
+}
+
+// Получаем форму и назначаем обработчик на событие отправки
+var form = document.querySelector('.form-data');
+
+if (form) {
+  form.addEventListener('submit', function (event) {
+    var enctype = form.getAttribute('enctype');
+
+    if (enctype === 'multipart/form-data') {
+      // form-data присутствует
+      if (!validateForm()) {
+        event.preventDefault(); // Отменяем отправку формы, если валидация не пройдена
+      }
+    } else {
+      // form-data отсутствует
+      event.preventDefault(); // Отменяем отправку формы
+      console.log("Форма не содержит form-data");
+    }
+
+    console.log("Успешно отправлено");
+  });
+} else {
+  console.log("Элемент формы не найден");
+}
+
+// Получаем поле телефона
+var phoneInput = document.querySelector('input[type="tel"]');
+
+// Проверяем, что поле телефона существует
+if (phoneInput) {
+  // Функция для применения маски к полю телефона
+  function applyPhoneMask(input) {
+    // Удаление всех символов, кроме цифр
+    var cleanedValue = input.value.replace(/\D/g, '');
+
+    // Добавление пробелов в маску
+    var formattedValue = cleanedValue.slice(0, 3) + ' ' + cleanedValue.slice(3, 5) + ' ' +
+      cleanedValue.slice(5, 7);
+
+    // Применение отформатированного значения обратно к полю ввода
+    input.value = formattedValue;
+  }
+
+  // Добавляем обработчик события ввода
+  phoneInput.addEventListener('input', function () {
+    applyPhoneMask(this);
+  });
+} else {
+  console.log("Поле телефона не найдено");
+}
+
+
+function instructionsList() {
+  const container = document.querySelector('.instructions-list');
+
+  if (!container) {
+    return null;
+  }
+
+  // Получаем все элементы с классом instructions-list__item-arrow
+  const arrows = document.querySelectorAll('.instructions-list__item-arrow');
+
+  // Перебираем каждый элемент и добавляем обработчик клика
+  arrows.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+      const item = arrow.closest('.instructions-list__item');
+      item.classList.toggle('active');
+    });
+  });
+
+  // Получаем все элементы с классами instructions-list__item-link и instructions-list__sublink
+  const links = document.querySelectorAll('.instructions-list__item-link, .instructions-list__sublink');
+
+  // Перебираем каждый элемент и добавляем обработчик клика
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      const items = document.querySelectorAll('.instructions-list__item.active');
+      items.forEach(item => {
+        item.classList.remove('active');
+      });
+    });
+  });
+
+  const burgerDefault = document.querySelector('[data-instructions-list-burger]');
+  const burgerClose = document.querySelector('[data-instructions-list-burger-close]');
+  // const body = document.querySelector('body')
+
+  burgerDefault.addEventListener('click', () => {
+    container.classList.add('active');
+    // body.classList.add('locked')
+  });
+
+  burgerClose.addEventListener('click', () => {
+    container.classList.remove('active');
+    // body.classList.remove('locked')
+  });
+
+  // Добавляем обработчик клика на объект document
+  document.addEventListener('click', (event) => {
+    const target = event.target;
+
+    // Проверяем, является ли целевой элемент бургером или элементом списка инструкций
+    const isBurger = target === burgerDefault || target === burgerClose;
+    const isInstructionsItem = target.closest('.instructions-list') === container;
+
+    // Если целевой элемент не является бургером и не является элементом списка инструкций,
+    // то удаляем классы active у контейнера и элементов списка
+    if (!isBurger && !isInstructionsItem) {
+      container.classList.remove('active');
+
+      const items = document.querySelectorAll('.instructions-list__item.active');
+      items.forEach(item => {
+        item.classList.remove('active');
+      });
+    }
+  });
+
+
+  // Перебираем каждый элемент и добавляем обработчик клика
+  links.forEach(link => {
+    link.addEventListener('click', (event) => {
+      const items = document.querySelectorAll('.instructions-list__item.active');
+      items.forEach(item => {
+        item.classList.remove('active');
+      });
+
+      container.classList.remove('active');
+
+      event.stopPropagation();
+    });
+  });
+
+}
+
+instructionsList();
+
+
+function container() {
+  const container = document.querySelector('.services')
+  const body = document.querySelector('body')
+
+  if (!container) {
+    return null
+  }
+
+  var timeoutId; // переменная для хранения идентификатора таймера
+
+  // Функция для удаления класса "active" с задержкой
+  function removeActiveClassWithDelay() {
+    timeoutId = setTimeout(function () {
+      container.classList.remove('show');
+      body.classList.remove('locked');
+    }, 500); // задержка в 0,5 секунды
+  }
+
+  // Добавление обработчика события при наведении на "services" и его дочерние элементы
+  container.addEventListener('mouseover', function () {
+    clearTimeout(timeoutId); // очистка таймера при наведении на элемент
+    container.classList.add('show');
+    body.classList.add('locked');
+  });
+
+  // Добавление обработчика события при уходе курсора с "services"
+  container.addEventListener('mouseout', removeActiveClassWithDelay);
+}
+
+if (window.matchMedia("(min-width: 991px)").matches) {
+  container()
+}
+
